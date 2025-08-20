dcf1e369e39968ffdc9c9eac83e52c091087113a
John-Katsikis <john.katsikis28@gmail.com>
2025-08-20 22:48:05 +0300

    Initial Commit

---
+++ VulkanTesting/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VulkanTesting
+//
+//  Created by John Katsikis on 20/08/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
